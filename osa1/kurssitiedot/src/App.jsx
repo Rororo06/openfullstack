542cc0bcaccffd4bec8c4f34c85c03b0303540ed
Taehee Han <taeheeusc@gmail.com>
2026-08-28 21:40:19 +0000

    Add part 1 exercises: kurssitiedot, unicafe, anekdootit

---
+++ osa1/kurssitiedot/src/App.jsx
@@ -0,0 +1,50 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part key={part.name} part={part} />
+    ))}
+  </div>
+)
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <p><b>Number of exercises {total}</b></p>
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
